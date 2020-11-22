8342cc59c55a7f3ef745c4800d470a1e583510de
Omwey228 <56200384+Omwey228@users.noreply.github.com>
2020-11-22 16:50:52 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,1 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ReApp
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
